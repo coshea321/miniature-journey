64fc134bdc5089139e09a0401b68adcc24ca4df3
coshea321 <cathal@oilean.com>
2026-05-05 23:48:54 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='health-v2';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','https://unpkg.com/react@18/umd/react.production.min.js','https://unpkg.com/react-dom@18/umd/react-dom.production.min.js'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{const u=new URL(e.request.url);if(u.origin===self.location.origin||u.hostname==='unpkg.com'){e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{if(r&&r.status===200){const cl=r.clone();caches.open(CACHE).then(ca=>ca.put(e.request,cl));}return r;})));}});
